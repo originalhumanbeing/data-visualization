c12b83ecaaa87119cb5c36e78df105fee36be4e1
originalhumanbeing <han.hyunjung17@gmail.com>
2018-11-29 23:41:01 +0900

    Add svg bars using v-for directive

---
+++ src/Chart.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="chart">
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        width="500" height="400" id="rect">
+        <g
+          v-for="(value, idx) in chartData"
+          :transform="`translate(0, ${idx * 40})`">
+          <rect height="30" width="100" fill="orange"></rect>
+          <text y="15">{{ value }}</text>
+        </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chart',
+  data () {
+    return {
+      chartData: [5, 8, 20, 3, 17, 14]
+    }
+  }
+}
+</script>
+
+<style>
+#chart {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#rect {
+}
+</style>
